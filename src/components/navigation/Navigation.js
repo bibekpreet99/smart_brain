8582c931dbf82b01322d2478ec36b2c6362f778b
bibekpreet99 <bibekpreet99@gmail.com>
2019-06-23 18:26:14 +0530

    added components

---
+++ src/components/navigation/Navigation.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navigation = ()=>{
+    return(
+        <nav style={{display: "flex", justifyContent: "flex-end"}}>
+            <p className="underline f3 pa3 link pointer black dim"> Sign Out </p>
+        </nav>
+    )
+}
+
+export default Navigation;
